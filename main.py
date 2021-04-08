9b1d7f9f93721003e7bd381ba8c904257498f298
Boris <boris.miodanic@gmail.com>
2021-04-08 15:39:29 +0200

    main cijeli

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-  
 import aiohttp
 import discord
 import os
@@ -6,3 +5,26 @@ from keys import bot as BOT_TOKEN
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix=".", owner_ids=[611540017000480773])
+
+@bot_event
+async def onready():
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game('koristi &help'))
+    print('Bot je online')
+
+@bot.command()
+@commands.is_owner()
+async def load(ctx, extension):
+    bot.load_extension(f'cogs.{extension}')
+    await ctx.send('\N{WHITE HEAVY CHECK MARK}')
+
+@bot.command()
+@commands.is_owner()
+async def unload(ctx, extension):
+    bot.unload_extension(f'cogs.{extension}')
+    await ctx.send('\N{WHITE HEAVY CHECK MARK}')
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+bot.run(BOT_TOKEN)
